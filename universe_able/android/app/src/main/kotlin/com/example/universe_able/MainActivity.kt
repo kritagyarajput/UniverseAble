da7ba8196368999946c85878f7d73f3f0159e078
Kritagya Rajput <67998608+kritagyarajput@users.noreply.github.com>
2021-02-27 17:12:53 +0530

    initiated

---
+++ universe_able/android/app/src/main/kotlin/com/example/universe_able/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.universe_able
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ universe_able/lib/DashboardScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello Shubhankar',
+          style: TextStyle(fontSize: 50, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
